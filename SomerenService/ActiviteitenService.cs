13b85fe7a1e45fadd7ca25e520a5b51aa5cfaa62
Berk075 <berk075@outlook.com>
2024-03-19 15:12:34 +0100

    Revert "Revert "Activiteiten werken nu""
    This reverts commit 12be04334164ee172fae19fa1f761ac01a7251b3.

---
+++ SomerenService/ActiviteitenService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SomerenDAL;
+using SomerenModel;
+
+namespace SomerenService
+{
+    public class ActiviteitenService
+    {
+        private ActiviteitDAO activiteitDAO;
+
+        public ActiviteitenService()
+        {
+            activiteitDAO = new ActiviteitDAO();
+        }
+
+        public List<Activiteiten> GetActivities()
+        {
+            List<Activiteiten> activities = activiteitDAO.GetAllActivities();
+            return activities;
+        }
+    }
+}
