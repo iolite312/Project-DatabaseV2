e5ce6bfeed35ef5555314dc70b899914e22479de
Toast3347 <706971@student.inholland.nl>
2024-03-21 20:14:31 +0100

    Assignment 3 finished
    Added the delete function :)

--- SomerenUI/DeleteDrink.Designer.cs
+++ SomerenUI/DeleteDrink.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DeleteDrink";
+            DeleteDrinkHeaderLbl = new System.Windows.Forms.Label();
+            DeleteDrinksEditedLbl = new System.Windows.Forms.Label();
+            DeleteDrinkEditBtn = new System.Windows.Forms.Button();
+            DeleteDrinkNametxt = new System.Windows.Forms.TextBox();
+            DeleteDrinkSelectCB = new System.Windows.Forms.ComboBox();
+            DrinkDeleteDrinkNameLbl = new System.Windows.Forms.Label();
+            DrinkDeleteDrinkIdLbl = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // DeleteDrinkHeaderLbl
+            // 
+            DeleteDrinkHeaderLbl.AutoSize = true;
+            DeleteDrinkHeaderLbl.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            DeleteDrinkHeaderLbl.Location = new System.Drawing.Point(357, 72);
+            DeleteDrinkHeaderLbl.Name = "DeleteDrinkHeaderLbl";
+            DeleteDrinkHeaderLbl.Size = new System.Drawing.Size(149, 35);
+            DeleteDrinkHeaderLbl.TabIndex = 30;
+            DeleteDrinkHeaderLbl.Text = "Delete drink";
+            // 
+            // DeleteDrinksEditedLbl
+            // 
+            DeleteDrinksEditedLbl.AutoSize = true;
+            DeleteDrinksEditedLbl.Location = new System.Drawing.Point(364, 277);
+            DeleteDrinksEditedLbl.Name = "DeleteDrinksEditedLbl";
+            DeleteDrinksEditedLbl.Size = new System.Drawing.Size(103, 20);
+            DeleteDrinksEditedLbl.TabIndex = 29;
+            DeleteDrinksEditedLbl.Text = "Drink deleted!";
+            // 
+            // DeleteDrinkEditBtn
+            // 
+            DeleteDrinkEditBtn.Location = new System.Drawing.Point(371, 245);
+            DeleteDrinkEditBtn.Name = "DeleteDrinkEditBtn";
+            DeleteDrinkEditBtn.Size = new System.Drawing.Size(94, 29);
+            DeleteDrinkEditBtn.TabIndex = 26;
+            DeleteDrinkEditBtn.Text = "Delete drink";
+            DeleteDrinkEditBtn.UseVisualStyleBackColor = true;
+            DeleteDrinkEditBtn.Click += DeleteDrinkEditBtn_Click;
+            // 
+            // DeleteDrinkNametxt
+            // 
+            DeleteDrinkNametxt.Location = new System.Drawing.Point(398, 176);
+            DeleteDrinkNametxt.Name = "DeleteDrinkNametxt";
+            DeleteDrinkNametxt.Size = new System.Drawing.Size(125, 27);
+            DeleteDrinkNametxt.TabIndex = 25;
+            // 
+            // DeleteDrinkSelectCB
+            // 
+            DeleteDrinkSelectCB.FormattingEnabled = true;
+            DeleteDrinkSelectCB.Location = new System.Drawing.Point(398, 142);
+            DeleteDrinkSelectCB.Name = "DeleteDrinkSelectCB";
+            DeleteDrinkSelectCB.Size = new System.Drawing.Size(125, 28);
+            DeleteDrinkSelectCB.TabIndex = 22;
+            DeleteDrinkSelectCB.SelectedIndexChanged += DeleteDrinkSelectCB_SelectedIndexChanged;
+            // 
+            // DrinkDeleteDrinkNameLbl
+            // 
+            DrinkDeleteDrinkNameLbl.AutoSize = true;
+            DrinkDeleteDrinkNameLbl.Location = new System.Drawing.Point(278, 183);
+            DrinkDeleteDrinkNameLbl.Name = "DrinkDeleteDrinkNameLbl";
+            DrinkDeleteDrinkNameLbl.Size = new System.Drawing.Size(52, 20);
+            DrinkDeleteDrinkNameLbl.TabIndex = 18;
+            DrinkDeleteDrinkNameLbl.Text = "Name:";
+            // 
+            // DrinkDeleteDrinkIdLbl
+            // 
+            DrinkDeleteDrinkIdLbl.AutoSize = true;
+            DrinkDeleteDrinkIdLbl.Location = new System.Drawing.Point(281, 142);
+            DrinkDeleteDrinkIdLbl.Name = "DrinkDeleteDrinkIdLbl";
+            DrinkDeleteDrinkIdLbl.Size = new System.Drawing.Size(25, 20);
+            DrinkDeleteDrinkIdLbl.TabIndex = 17;
+            DrinkDeleteDrinkIdLbl.Text = "Id:";
+            // 
+            // DeleteDrink
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(DeleteDrinkHeaderLbl);
+            Controls.Add(DeleteDrinksEditedLbl);
+            Controls.Add(DeleteDrinkEditBtn);
+            Controls.Add(DeleteDrinkNametxt);
+            Controls.Add(DeleteDrinkSelectCB);
+            Controls.Add(DrinkDeleteDrinkNameLbl);
+            Controls.Add(DrinkDeleteDrinkIdLbl);
+            Name = "DeleteDrink";
+            Text = "DeleteDrink";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label DeleteDrinkHeaderLbl;
+        private System.Windows.Forms.Label DeleteDrinksEditedLbl;
+        private System.Windows.Forms.Button DeleteDrinkEditBtn;
+        private System.Windows.Forms.TextBox DeleteDrinkNametxt;
+        private System.Windows.Forms.ComboBox DeleteDrinkSelectCB;
+        private System.Windows.Forms.Label DrinkDeleteDrinkNameLbl;
+        private System.Windows.Forms.Label DrinkDeleteDrinkIdLbl;
     }
 }
--- SomerenUI/DeleteDrink.cs
+++ SomerenUI/DeleteDrink.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SomerenModel;
+using SomerenService;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SomerenModel;
+using SomerenService;
 
 namespace SomerenUI
 {
     public partial class DeleteDrink : Form
     {
+        DrinksService drinksService;
+        List<Drinks> drinks;
+        Drinks drink;
         public DeleteDrink()
         {
             InitializeComponent();
+            DeleteDrinksEditedLbl.Visible = false;
+            drinksService = new DrinksService();
+            drinks = drinksService.GetDrinks();
+            foreach (Drinks drink in drinks)
+            {
+                DeleteDrinkSelectCB.Items.Add(drink.Id);
+            }
+        }
+
+        private void DeleteDrinkSelectCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string drinkIdString = DeleteDrinkSelectCB.SelectedItem.ToString();
+            int drinkId = int.Parse(drinkIdString);
+            drink = drinksService.GetDrinkById(drinkId);
+            DeleteDrinkNametxt.Text = drink.name.ToString();
+        }
+
+        private void DeleteDrinkEditBtn_Click(object sender, EventArgs e)
+        {
+            DeletingDrink();
+
+        }
+
+        private void DeletingDrink()
+        {
+            try
+            {
+                string drinkIdString = DeleteDrinkSelectCB.SelectedItem.ToString(); int drinkId = int.Parse(drinkIdString); string name = DeleteDrinkNametxt.Text.ToString();
+                drinksService.RemoveDrinks(drink);
+                DeleteDrinksEditedLbl.Visible = true;
+            }
+            catch (Exception ex) { MessageBox.Show($"Error: {ex.Message}"); }
         }
     }
 }
